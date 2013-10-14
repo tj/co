ecc889649b2486c481eb726bc75aa36832d421fd
TJ Holowaychuk <tj@vision-media.ca>
2013-10-14 10:45:00 -0700

    fix race in parallel.js example
    this is why node shouldnt use callbacks in core :D

--- examples/parallel.js
+++ examples/parallel.js
@@ -4,23 +4,38 @@ var fs = require('fs');
 
 // by writing this a bit differently than ./simple.js
 // you can acheive parallel async operations simply
-// be deferring the use of "yield" to act as the callback
+// be deferring the use of "yield" to act as the callback.
 
 // alternatively you could use ./simple.js with
 // var res = yield [a, b, c] to execute them in
 // parallel.
 
 // for an alternative see ./join.js
+// also if writing all this boilerplate
+// is not to your liking check out "thunkify"
 
 function read(file) {
-  var done;
+  var finished;
+  var error;
+  var value;
+  var cb;
 
-  fs.readFile(file, 'utf8', function(err, val){
-    done(err, val);
+  fs.readFile(file, 'utf8', function(err, str){
+    if (cb) {
+      cb(err, str);
+    } else {
+      finished = true;
+      error = err;
+      value = str;
+    }
   });
 
   return function(fn){
-    done = fn;
+    if (finished) {
+      fn(error, value);
+    } else {
+      cb = fn;
+    }
   }
 }
 
