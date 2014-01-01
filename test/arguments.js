d0cdca7a3b905099f8f21d3870e94846c94914c1
Jake Luer <jake@alogicalparadox.com>
2014-01-01 04:09:41 -0500

    bug: nil arguments replaced with error fn

--- test/arguments.js
+++ test/arguments.js
@@ -6,4 +6,10 @@ describe('co()(args...)', function(){
   it('should not pass the thunk as an arguments', co(function *(){
     assert.equal(arguments.length, 0);
   }))
+
+  it('should not pass error for nil first argument', function(done){
+    co(function *(i){
+      assert.equal(i, 0);
+    })(0, done);
+  });
 })
