c360d999d20ccdf0a7722fbee5817a972dabb214
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2014-03-17 00:46:30 +0200

    Fix #98 - treat Date objects as primitives

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ function toThunk(obj, ctx) {
     return obj;
   }
 
-  if (obj && 'object' == typeof obj) {
+  if (obj && 'object' == typeof obj && toString.call(obj) !== '[object Date]') {
     return objectToThunk.call(ctx, obj);
   }
 
